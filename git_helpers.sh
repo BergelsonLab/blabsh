1890307298021d0d480d6e25f9f970f5b98b352f
Zhenya <e.kalenkovich@gmail.com>
2025-05-22 11:40:42 -0400

    feat+refactor: add git_recreat-merge, rename git helper funs

--- git_helpers.sh
+++ git_helpers.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # get four sides of a merge
-extract_merge_files() {
+git_extract-merge-files() {
     local commit=$1
     local path=$2
     git show $(git merge-base $commit^1 $commit^2):$path > $path.BASE
@@ -11,9 +11,18 @@ extract_merge_files() {
 }
 
 # get three sides of a merge conflict
-extract_conflict_files() {
+git_extract-conflict-files() {
    local path=$1
    git show $(git merge-base HEAD MERGE_HEAD):$path > $path.BASE
    git show HEAD:$path > $path.OURS
    git show MERGE_HEAD:$path > $path.THEIRS
+}
+
+git_recreate-merge() {
+   local commit=$1
+   local path=$2
+   git branch $path.OURS $commit^1
+   git branch $path.THEIRS $commit^2
+   git switch $path.OURS
+   git merge $path.THEIRS -m "temp: merge troubleshooting"
 }
